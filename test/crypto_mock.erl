3c2e7f5885f5f03025f5fa00fd2c217c1553b268
pulitta <pulitta11@gmail.com>
2016-08-30 23:39:07 +0300

    code formatted

--- test/crypto_mock.erl
+++ test/crypto_mock.erl
@@ -3,6 +3,6 @@
 -export([encrypt/2]).
 
 encrypt(Key, Data) when is_binary(Key) andalso is_binary(Data) ->
-%%Key matters here, even though it’s not used.
-timer:sleep(2000),
-Data.
+    %%Key matters here, even though it’s not used.
+    timer:sleep(2000),
+    Data.
