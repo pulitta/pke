a122a6e1c429a64b6cf8104a8b022b1a9e27a9bf
pulitta <pulitta11@gmail.com>
2016-08-30 23:24:53 +0300

    config fixed

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :pke, 
-key_path: "test/ke.pub",		# <- Public key path
+key_path: "test/key.pub",		# <- Public key path
 crypto_module: :crypto_mock		# <- Crypto module name
 
