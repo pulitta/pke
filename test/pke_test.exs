793be1fa13821fcd5c4e0b171cac16a858d389f6
pulitta <pulitta11@gmail.com>
2016-08-31 01:05:12 +0300

    test added

--- test/pke_test.exs
+++ test/pke_test.exs
@@ -1,8 +1,40 @@
 defmodule PkeTest do
-  use ExUnit.Case
-  doctest Pke
+    use ExUnit.Case
+    doctest Pke
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+    setup do
+        Application.delete_env(:pke, :key_path, [])
+        Application.delete_env(:pke, :crypto_module, [])
+    end
+
+    test "No data args in command line" do
+        Application.put_env(:pke, :key_path, "test/key.pub", [])
+        Application.put_env(:pke, :crypto_module, :crypto_mock, [])
+        result = Pke.main([])
+        assert nil == result
+    end
+
+    test "No key_path in config" do
+        result = Pke.main(["--data", "123"])
+        assert nil == result
+    end
+
+    test "No crypto module in config" do
+        Application.put_env(:pke, :key_path, "test/key.pub", [])
+        result = Pke.main(["--data", "123"])
+        assert nil == result
+    end
+
+    test "No key file" do
+        Application.put_env(:pke, :key_path, "test.pub", [])
+        result = Pke.main(["--data", <<"123">>])
+        assert nil == result
+    end
+
+    test "Success" do
+        Application.put_env(:pke, :key_path, "test/key.pub", [])
+        Application.put_env(:pke, :crypto_module, :crypto_mock, [])
+        result = Pke.main(["--data", "123"])
+        assert :ok == result
+    end
 end
